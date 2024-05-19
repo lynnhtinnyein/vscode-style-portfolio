b599add827159dd6e7e5ee2880b53b0b6a9c5313
lynnhtinnyein <lynnhtinnyein@gmail.com>
2024-05-19 22:53:09 +0700

    Fixed for deployment 3

--- src/pages/TabBar/partials/Tab.js
+++ src/pages/TabBar/partials/Tab.js
@@ -8,23 +8,7 @@ const Tab = ({ id, isActive, onClick, onClose }) => {
                 isActive ? 'bg-zinc-800' : 'bg-zinc-700'
             }`}
         >
-            <div
-                className="flex flex-row space-x-2 py-2 pl-3"
-                onClick={onClick}
-            >
-                { pages.find((e) => e.id === id).fileIcon }
-
-                <span className="text-yellow-200 text-xs">
-                    { pages.find((e) => e.id === id).name }
-                </span>
-            </div>
-
-            <div
-                className="hover:bg-gray-500 rounded"
-                onClick={onClose}
-            >
-                <XIcon size="14" />
-            </div>
+            <span>{id}</span>
         </div>
     );
 };
